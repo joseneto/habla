0451de189f132b2764731dc1d6cfc25a36889dcd
Jose Neto <jmedeiros.neto@gmail.com>
2020-04-05 01:21:15 -0300

    realtime chat version 0.1

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const	express	=	require('express');
 const	path	=	require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const	consign	=	require('consign');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session  = require('express-session');
 const methodOverride = require('method-override');
 const error = require('./middlewares/error');
+
 const	app	=	express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine',	'ejs');
@@ -33,12 +38,19 @@ consign({})
   .then('controllers')
   .then('routes')
   .into(app);
-
-
 //middlewares de error
 app.use(error.notFound);
 app.use(error.serverError);
 
-app.listen(3000,	()	=>	{
+io.on('connection', (client) => {
+  client.on('send-server', (data) => {
+  
+    const response = data;
+    client.emit('send-client', response);
+    client.broadcast.emit('send-client', response);
+  });
+}); 
+
+server.listen(3000,	()	=>	{
   console.log('Habla running!');
 });
