e617c3e96cbf4b8a13954a73ba6030844c0da0af
Jose Neto <jmedeiros.neto@gmail.com>
2020-04-09 16:38:54 -0300

    redis message track

--- events/chat.js
+++ events/chat.js
@@ -1,42 +1,63 @@
+const redis = require('redis').createClient();
+
 module.exports = (app, io) => {
-    const onlines = [];
-    let disconnectMessage =  {id: -1, name: "user", text: "disconnect" ,time: new Date(), avatar: "", position:"left"};
+    
+    let defaultMessage =  {id: -1, name: "user", text: "disconnect" ,time: new Date(), avatar: "", position:"left"};
     io.on('connection', (client) => {
 
         const { session } =	client.handshake;
         const { user } = session;
 
-        onlines[user.name] = user.name;
-
-        for(let name in onlines){
-            client.emit('notify-onlines', name);
-            client.broadcast.emit('notify-onlines', name);
-        }
+        redis.sadd('onlines', user._id, () => {
+            redis.smenbers('onlines',(err, ids) => {
+                ids.forEach((id) => {
+                    client.emit('notify-onlines', id);
+                    client.broadcast.emit('notify-onlines',	id);
+                });
+            })
+        });
 
         client.on('send-server', (room, data) => {
-          const response = data;
-          const newMessage = {name: data.name, room: room};
+          let response = data;
+          const newMessage = {idReg: user._id, room: room};
           session.room = room;
-          
+          response.idReg = user._id;
+          redis.lpush(room, response);
           client.broadcast.emit('new-message', newMessage);
           io.to(room).emit('send-client', response);
          
         });
 
         client.on('create-room', (data) => {
-        
+            const randomId = Math.random();
             session.room = data;
             client.join(data);
+            defaultMessage.id= randomId;    
+            defaultMessage.text='User enter the chat';
+            defaultMessage.name=user.name;
+            redis.lpush(data, defaultMessage, () => {
+                redis.lrange(data, 0, -1, (err, msgs) => {
+                    msgs.forEach((msg) => {
+                        io.to(data).emit('send-client', msg);
+                    });
+                });
+            });
+
         });
 
         client.on('disconnect', () => {
+            const randomId = Math.random();
             const {room} = session;
-            delete onlines[user.name];
-            session.room = null;
-            
-            client.broadcast.emit('notify-offlines', user.name);
-            io.to(room).emit('send-client', disconnectMessage);
-            client.leave(room);
+          
+            defaultMessage.id= randomId;  
+            redis.lpush(room, defaultMessage, () => {
+                session.room = null;
+                redis.srem('onlines', user._id);
+                client.broadcast.emit('notify-offlines', user.name);
+                io.to(room).emit('send-client', defaultMessage);
+                client.leave(room);
+            });
+          
         });
 
     }); 
